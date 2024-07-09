7be5ec136ffccd4cebe7bd5bf1a7ac069cfbc189
양시연 <yangsiyeon@yangsiyeons-MacBook-Pro.local>
2024-07-09 18:25:15 +0900

    add: 초기 설정

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import classes from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={classes.mainWrapper}>
+      <h1 className={classes.title}>엑스플랫 퍼블리싱 라이브러리 </h1>
+    </main>
+  );
+}
