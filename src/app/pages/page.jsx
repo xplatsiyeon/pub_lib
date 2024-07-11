b9b2c8f31b93d7ba84c9c1fe117daeb66ef6668e
양시연 <yangsiyeon@yangsiyeons-MacBook-Pro.local>
2024-07-11 15:42:38 +0900

    feat: 페이지 카테고리 루트 레이아웃 생성

--- src/app/pages/page.jsx
+++ src/app/pages/page.jsx
@@ -1,5 +1,26 @@
+import React from "react";
+
 import classes from "./Pages.module.scss";
 
+import Header from "@/component/layout/Header";
+import Footer from "@/component/layout/Footer";
+import Grid from "@/component/pages/main/Grid";
+
 export default function Pages() {
-  return <div>페이지 페이지</div>;
+  return (
+    <div className={classes.pagesWrapper}>
+      <article className={classes.header}>
+        <h2 className={classes.heading}>헤더</h2>
+        <Header />
+      </article>
+      <article>
+        <h2 className={classes.heading}>푸터</h2>
+        <Footer />
+      </article>
+      <article>
+        <h2 className={classes.heading}>그리드</h2>
+        <Grid />
+      </article>
+    </div>
+  );
 }
