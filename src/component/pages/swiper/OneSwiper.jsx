6315c8fa54fc0347c14840f1cf952a6b29382299
양시연 <yangsiyeon@yangsiyeons-MacBook-Pro.local>
2024-07-16 12:33:27 +0900

    feat: OneSwiper 컴포넌트 생성

---
+++ src/component/pages/swiper/OneSwiper.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import classes from "./OneSwiper.module.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css/navigation";
+
+const items = [
+  { color: "green" },
+  { color: "purple" },
+  { color: "black" },
+  { color: "pink" },
+  { color: "skyblue" },
+  { color: "tomato" },
+  { color: "grey" },
+];
+
+export default function OneSwiper() {
+  return (
+    <Swiper
+      className={classes.oneSwiper}
+      modules={[Pagination, Navigation]}
+      pagination={{ clickable: true }}
+      navigation={true}
+    >
+      {items.map((item, index) => {
+        return (
+          <SwiperSlide
+            className={classes.swiperItem}
+            key={index}
+            style={{ color: item.color }}
+          >
+            {index + 1}번째 아이템
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+}
