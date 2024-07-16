0222a953fec2df39e5fc3954528e5d0f71b6ec18
양시연 <yangsiyeon@yangsiyeons-MacBook-Pro.local>
2024-07-16 18:40:28 +0900

    oneSwiper의 pagination 클릭 가능하게 변경

--- src/component/pages/swiper/OneSwiper.jsx
+++ src/component/pages/swiper/OneSwiper.jsx
@@ -5,6 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const items = [
   { color: "green" },
