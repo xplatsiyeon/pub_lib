787bd7eb409d2fdd1e2ecb6b416f16ff89022cf6
양시연 <yangsiyeon@yangsiyeons-MacBook-Pro.local>
2024-07-17 11:12:22 +0900

    feat: loop 페이지 생성 후 컴포넌트 불러오기

---
+++ src/app/pages/loop/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import classes from "./Loop.module.scss";
+import ImageLoop from "@/component/pages/loop/ImageLoop";
+import TextLoop from "@/component/pages/loop/TextLoop";
+
+export default function Loop() {
+  return (
+    <div className={classes.loopWrapper}>
+      <article className="imageLoop">
+        <h2>이미지 무한 루프</h2>
+        <ImageLoop />
+      </article>
+      <article className="textLoop">
+        <h2>텍스트 무한 루프</h2>
+        <TextLoop />
+      </article>
+    </div>
+  );
+}
